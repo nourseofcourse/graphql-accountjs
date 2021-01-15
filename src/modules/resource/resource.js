47fa371ed346ac89104c1e9b40ffe163c9c74fe7
Brandon Nourse <nourseofcourse@gmail.com>
2021-01-14 22:14:41 -0500

    adding workship graphql data

---
+++ src/modules/resource/resource.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+
+const resourceSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      maxlength: 255,
+      trim: true
+    },
+    slug: {
+      type: String,
+      maxlength: 255,
+      trim: true
+    },
+    description: {
+      type: String,
+      maxlength: 16777215,
+      trim: true,
+      default: ""
+    },
+    link: {
+      type: String,
+      maxlength: 255,
+      default: ""
+    },
+    public: {
+      type: Boolean,
+      default: true
+    },
+    image_type: {
+      type: String,
+      maxlength: 255,
+      trim: true
+    },
+    image: {
+      type: String,
+      maxlength: 255,
+      trim: true
+    }
+    // context_id: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   required: true,
+    //   refPath: 'onModel'
+    // },
+    // context: {
+    //   type: String,
+    //   required: true,
+    //   enum: ['Course', 'Lesson']
+    // }
+  },
+  { timestamps: true }
+)
+
+//resourceSchema.index({ slug: 1 }, { unique: true })
+
+export const Resource = mongoose.model('Resource', resourceSchema)
---
+++ src/modules/workshop/index.js
@@ -0,0 +1,100 @@
+const gql = require('graphql-tag')
+
+const typeDefs = gql`
+
+  enum WorkshopStatus {
+    ACTIVE
+    FUTURE
+    DRAFT
+    TRASH
+    INACTIVE
+  }
+
+  enum WorkshopType {
+    SOCIALSECURITY
+  }
+
+  enum RegistrationStatus {
+    REGISTERED
+    ABANDONED
+    CANCELLED
+  }
+
+  type Workshop {
+    id: ID!
+    name: String!
+    agent: User!
+    start_date: String
+    end_date: String
+    type: WorkshopType
+    venues: [Venue]
+    template: WorkshopTemplate
+    status: WorkshopStatus
+    organizers: [User]
+    attendees: [WorkshopAttendee]
+  }
+
+  type WorkshopAttendee {
+    id: ID!
+    workshop_id: Workshop!
+    status: RegistrationStatus
+  }
+
+  type WorkshopTemplate {
+    id: ID!
+    name: String!
+    preview_url: String
+    fields: Array
+  }
+
+  type Country {
+    id: ID!
+    name: String!
+    code: String!
+  }
+
+  type State {
+    id: ID!
+    name: String!
+    code: String!
+  }
+
+  type Venue {
+    id: ID!
+    name: String!
+    about: String
+    image: String
+    address: String!
+    address_2: String
+    city: String
+    state: State
+    zip: int
+    country: Country
+  }
+
+  type Query {
+    venue(id: ID!): Venue!
+    venues: [Venue]
+    workshop(id: ID!): Workshop!
+    workshops: [Workshop]
+    state(id: ID!): State!
+    states: [States]
+    country(id: ID!): Country!
+    countries: [Country]
+    template(id: ID!): WorkshopTemplate!
+    templates: [WorkshopTemplate]
+    attendee(id: ID!): WorkshopAttendee!
+    attendees: [WorkshopAttendee]
+  }
+
+  type Mutation {
+
+  }
+`
+
+const resolvers = require('./resolvers')
+
+module.exports = {
+  typeDefs,
+  resolvers
+}
